6407ee764e18bc940f6071180d3bfa9e57f58274
putrimail <syarowiputri@gmail.com>
2024-12-06 17:09:05 +0800

    update

--- src/controllers/vendorController.js
+++ src/controllers/vendorController.js
@@ -115,6 +115,8 @@ export const login = async (req, res) => {
     );
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
+      secure: true,
+      sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000, // 1 hari
     });
 
